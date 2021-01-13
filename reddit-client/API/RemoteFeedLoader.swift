b0910165a4eb1eee463c8e96950603f0503d4bde
Francisco Conelli <fconelli@gmail.com>
2021-01-13 08:24:06 -0300

    Make HTTPClient a protocol

--- reddit-client/API/RemoteFeedLoader.swift
+++ reddit-client/API/RemoteFeedLoader.swift
@@ -7,19 +7,26 @@
 
 import Foundation
 
+/// protocol for declaring extra functionality needed by RemoteFeedLoader.
+/// The idea is to make a URLSession extension that conforms to this so the loader does not depend on concrete classes and can be more flexible and easier to test
+public protocol HTTPClient {
+    func get(from url: URL)
+}
+
 public final class RemoteFeedLoader: FeedLoader {
     
     private let client: HTTPClient
+    private let url: URL = URL(string: "www.reddit.com/top")!
     
     public init(client: HTTPClient) {
         self.client = client
     }
     
     func load(completion: @escaping (LoadFeedResult) -> Void) {
-        client.requestedURL = URL(string: "www.reddit.com/top")
+        client.get(from: url)
     }
-}
-
-public class HTTPClient {
-    var requestedURL: URL?
+    
+    func getURL() -> URL {
+        return self.url
+    }
 }
